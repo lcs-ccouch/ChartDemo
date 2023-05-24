92664ea5e6b215b623ceb21e5c9c9c68f4c3c5d3
Calum Couch <ccouch@lcs.on.ca>
2023-05-24 14:02:58 -0400

    Initial Commit

---
+++ ChartDemo/ChartDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartDemoApp.swift
+//  ChartDemo
+//
+//  Created by calum couch on 2023-05-24.
+//
+
+import SwiftUI
+
+@main
+struct ChartDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
